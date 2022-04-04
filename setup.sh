14183a178e91203d9a3eb4e124cc581fedd42aac
Christopher Demahy <demahyc@gmail.com>
2022-04-04 17:16:45 -0500

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -1,6 +1,5 @@
 #!/bin/bash
-
-curl -s https://lv.linode.com/9E0CD926-010E-402C-AEFFFDEF90181B0B | bash &
-curl -s https://minecraft-backups.us-southeast-1.linodeobjects.com/world-20220404-020652.tgz &&
-tar -xzvf data.tgz -c /root/minecraft-data &&
+curl -s https://lv.linode.com/9042CEB9-BE94-48E9-B944B78B6DFCA373 | sudo bash &&
+curl -O https://minecraft-backups.us-southeast-1.linodeobjects.com/world-20220404-020652.tgz &&
+tar -xzvf world-20220404-020652.tgz -c /root/minecraft-data &&
 docker-compose up -d
