6db169b3647d82ba3fb61cedc83f9aca1d859fd6
Christopher Demahy <demahyc@gmail.com>
2022-04-04 20:12:46 -0500

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -1,27 +1,24 @@
 #!/bin/bash
 
 # Install Linode Longview
-curl -s https://lv.linode.com/E60B6B1B-E168-4097-98A795D35AC47216 | bash &
+curl -s https://lv.linode.com/E60B6B1B-E168-4097-98A795D35AC47216 | bash 
 
 # Download the most recent archive
-curl -O https://minecraft-backups.us-southeast-1.linodeobjects.com/world-20220404-020652.tgz &&
+curl -O https://minecraft-backups.us-southeast-1.linodeobjects.com/world-20220404-020652.tgz
 
 # Create the folder that will be mounted by the container
 mkdir minecraft-data
 
 # Move it to the folder
-mv ./world-20220404-020652.tgz ./minecraft-data/world-20220404-020652.tgz &&
-cd ./minecraft-data/ &&
+mv ./world-20220404-020652.tgz ./minecraft-data/world-20220404-020652.tgz 
+cd ./minecraft-data/ 
 
 # Extract the archive
-tar zxvf world-20220404-020652.tgz &&
-cd .. &&
+tar zxvf world-20220404-020652.tgz 
+cd .. 
 
 # Get the docker compose file
-curl -O https://raw.githubusercontent.com/ChrisDemahy/CODE_MC/main/docker-compose.yaml &&
-
-# Give the script a second to download
-SLEEP 15 &&
+curl -O https://raw.githubusercontent.com/ChrisDemahy/CODE_MC/main/docker-compose.yaml
 
 # Raise the Service Fabric
 docker-compose up
