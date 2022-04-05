693033f12892e6673b4d7a0e007999ce493e69d9
Christopher Demahy <demahyc@gmail.com>
2022-04-04 20:46:01 -0500

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 # Install Linode Longview
-curl -s https://lv.linode.com/67DB7F58-050D-4649-A7522CA0E291C78D | bash 
+curl -s https://lv.linode.com/7AB000CC-09D9-45FE-8BF2790A33CB7420 | bash 
 
 # Download the most recent archive
 curl -O https://minecraft-backups.us-southeast-1.linodeobjects.com/world-20220404-020652.tgz
@@ -14,17 +14,8 @@ mv ./world-20220404-020652.tgz ./minecraft-data/world-20220404-020652.tgz
 cd ./minecraft-data/ 
 
 # Extract the archive
-tar zxvf world-20220404-020652.tgz 
+tar zxvf world-20220404-020652.tgz
 cd .. 
-
-# Get the docker compose file
-curl -O https://raw.githubusercontent.com/ChrisDemahy/CODE_MC/main/docker-compose.yaml
-
-# write to the console
-echo "SLEEPING FOR 30 SECONDS"
-
-# Sleep to let it download
-SLEEP 30
 
 # Raise the Service Fabric
 docker-compose up
