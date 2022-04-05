7118a140226f3962e7e11e223863e7b015ae16b3
ChrisDemahy <demahyc@gmail.com>
2022-04-04 20:17:20 -0500

    working

--- setup.sh
+++ setup.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 # Install Linode Longview
-curl -s https://lv.linode.com/E60B6B1B-E168-4097-98A795D35AC47216 | bash 
+curl -s https://lv.linode.com/67DB7F58-050D-4649-A7522CA0E291C78D | bash 
 
 # Download the most recent archive
 curl -O https://minecraft-backups.us-southeast-1.linodeobjects.com/world-20220404-020652.tgz
